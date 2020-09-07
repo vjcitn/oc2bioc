f7a61e0a9ebc2bb0f6f0ae0d9cc2431fb8028f27
Vince Carey <stvjc@channing.harvard.edu>
2020-09-07 08:21:01 -0400

    vignette progress with serialized responses

--- R/query.R
+++ R/query.R
@@ -8,10 +8,10 @@ logoutOC = function()
 
 #' single-variant query
 #' @import httr
-#' @param uname user name registered at run.opencravat.org
-#' @param passwd password for opencravat account
+#' @param uname user name registered at run.opencravat.org; will use value of environment variable OCRAVAT_USERNAME if missing
+#' @param passwd password for opencravat account; will use value of environment variable OCRAVAT_PASSWORD if missing
 #' @param chr character(1) chromosome in chrn format
-#' @param pos character(1) position
+#' @param pos character(1) position, in hg38 coordinates
 #' @param ref character(1) reference base
 #' @param alt character(1) alternate base
 #' @param annotators character() vector of annotator tags
@@ -20,6 +20,9 @@ logoutOC = function()
 #' @export
 queryOC = function(uname, passwd, chr="chr1", pos="12777320", ref="G", alt="T",
   annotators = c("clinvar", "dbsnp", "exac_gene", "go", "rvis")) {
+  if (missing(uname)) uname = Sys.getenv("OCRAVAT_USERNAME")
+  if (missing(passwd)) passwd = Sys.getenv("OCRAVAT_PASSWORD")
+  if (nchar(uname)==0) stop("no username available")
   loginOC(uname=uname, passwd=passwd)
   on.exit(logoutOC())
   q = paste0("https://run.opencravat.org/submit/annotate?",
