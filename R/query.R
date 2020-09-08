a7a8fb0abd4b13ad47d2b5b4c901ec666464dbad
Vince Carey <stvjc@channing.harvard.edu>
2020-09-08 15:47:09 -0400

    introduce material on API for local deployment

--- R/query.R
+++ R/query.R
@@ -15,17 +15,25 @@ logoutOC = function()
 #' @param ref character(1) reference base
 #' @param alt character(1) alternate base
 #' @param annotators character() vector of annotator tags
+#' @note The default value of `baseURL` entails that this function
+#' will query run.opencravat.org.  A username and password must
+#' be supplied in this case.  For other values of `baseURL`, no
+#' authentication is attempted at this time.  For a local instance
+#' of OpenCRAVAT, `http://0.0.0.0:8080` is a default setting for
+#' baseURL.
 #' @examples
 #' queryOC # don't want to use auth
 #' @export
-queryOC = function(uname, passwd, chr="chr1", pos="12777320", ref="G", alt="T",
+queryOC = function(baseURL="https://run.opencravat.org", uname, passwd, chr="chr1", pos="12777320", ref="G", alt="T",
   annotators = c("clinvar", "dbsnp", "exac_gene", "go", "rvis")) {
-  if (missing(uname)) uname = Sys.getenv("OCRAVAT_USERNAME")
-  if (missing(passwd)) passwd = Sys.getenv("OCRAVAT_PASSWORD")
-  if (nchar(uname)==0) stop("no username available")
-  loginOC(uname=uname, passwd=passwd)
-  on.exit(logoutOC())
-  q = paste0("https://run.opencravat.org/submit/annotate?",
+  if (baseURL == "https://run.opencravat.org") {
+    if (missing(uname)) uname = Sys.getenv("OCRAVAT_USERNAME")
+    if (missing(passwd)) passwd = Sys.getenv("OCRAVAT_PASSWORD")
+    if (nchar(uname)==0) stop("no username available and baseURL is run.opencravat.org")
+    loginOC(uname=uname, passwd=passwd)
+    on.exit(logoutOC())
+    }
+  q = paste0(baseURL, "/submit/annotate?",
        "chrom=", chr, "&pos=", pos, "&ref_base=", ref, "&alt_base=", alt,
        "&annotators=", paste(annotators, collapse=","))
   httr::GET(q)
