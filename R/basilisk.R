be933a00465e6cfc32b2ca3a34a2a63c18b613ce
Vince Carey <stvjc@channing.harvard.edu>
2020-09-29 10:19:27 -0400

    bump pip version for open-cravat

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,4 +2,4 @@
 ocbenv <- basilisk::BasiliskEnvironment(envname="ocbenv",
     pkgname="oc2bioc", packages=c("python==3.8.2", "requests==2.22.0", 
       "pytz==2019.3"),
-    pip=c("open-cravat==2.1.0"))
+    pip=c("open-cravat==2.1.1"))
