c92a8a273935e57fa33e75dc3570e72ad430defd
vjcitn <stvjc@channing.harvard.edu>
2024-09-06 18:41:36 -0400

    updating for 2024

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,5 @@
 
 ocbenv <- basilisk::BasiliskEnvironment(envname="ocbenv",
-    pkgname="oc2bioc", packages=c("python=3.8.2", "requests==2.22.0", 
-      "pytz==2019.3"),
-    pip=c("open-cravat==2.2.1"))
+    pkgname="oc2bioc",
+    packages="chardet==3.0.4",
+    pip=c("open-cravat==2.8.0"))
